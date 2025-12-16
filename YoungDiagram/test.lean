37d0f07a7e52af0d062a03bf96c97a9e17efa821
Yu-Misaka <109365723+Yu-Misaka@users.noreply.github.com>
2025-12-16 16:24:29 +0800

    some attempts to define

---
+++ YoungDiagram/test.lean
@@ -0,0 +1,159 @@
+import Mathlib
+
+open Finsupp
+
+inductive GeneType
+| NonPolarized
+| Positive
+| Negative
+deriving DecidableEq, Repr
+
+structure Gene where
+  rank : ℕ
+  type : GeneType
+  rank_pos : 1 ≤ rank
+deriving DecidableEq, Repr
+
+section polarized
+
+abbrev List.isAlt {l : List Bool} (hl : l ≠ [] := by decide) : Prop :=
+  l = List.iterate not (l.head hl) l.length
+
+#eval [true].isAlt
+#eval [true, true].isAlt
+#eval [true, false].isAlt
+#eval [true, false, true].isAlt
+#eval [false, true, false].isAlt
+
+def List.toGen {l : List Bool} (hl : l ≠ [] := by decide)
+    (_ : l.isAlt hl := by decide) : Gene :=
+  ⟨l.length, if l.head hl = true then .Positive else .Negative, List.length_pos_iff.2 hl⟩
+
+#eval [true].toGen
+#eval [true, false].toGen
+#eval [true, false, true].toGen
+#eval [false, true, false].toGen
+
+def Gene.toList {g : Gene} (_ : g.type ≠ .NonPolarized := by decide) : List Bool :=
+  List.iterate not
+    (match g.type with | .Positive => true | .Negative => false | .NonPolarized => by tauto)
+    g.rank
+
+#eval [true].toGen.toList
+#eval [true, false].toGen.toList
+#eval [true, false, true].toGen.toList
+#eval [false, true, false].toGen.toList
+
+end polarized
+
+abbrev Chromosome := Gene →₀ ℕ
+
+namespace Chromosome
+
+#check Prod.le_def
+#check Prod.instPartialOrder
+
+def geneSignature (g : Gene) : ℚ × ℚ :=
+  let n := (g.rank : ℚ)
+  match g.type with
+  | .NonPolarized => (n / 2, n / 2)
+  | .Positive =>
+      if g.rank % 2 == 0 then (n / 2, n / 2)
+      else ((n + 1) / 2, (n - 1) / 2)
+  | .Negative =>
+      if g.rank % 2 == 0 then (n / 2, n / 2)
+      else ((n - 1) / 2, (n + 1) / 2)
+
+def signature (c : Chromosome) : ℚ × ℚ :=
+  c.sum (fun g count ↦ (count : ℚ) • geneSignature g)
+
+noncomputable def primeGene (g : Gene) : Chromosome :=
+  if h : g.rank > 1 then
+    let newGene : Gene := {
+      rank := g.rank - 1,
+      type := g.type,
+      rank_pos := Nat.le_sub_one_of_lt h
+    }
+    single newGene 1
+  else
+    0
+
+noncomputable def prime (c : Chromosome) : Chromosome :=
+  c.sum (fun g m => m • primeGene g)
+
+noncomputable def iteratePrime (c : Chromosome) (k : ℕ) : Chromosome :=
+  match k with
+  | 0 => c
+  | k + 1 => prime (iteratePrime c k)
+
+def dominates (X Y : Chromosome) : Prop :=
+  ∀ k : ℕ, signature (iteratePrime Y k) ≤ signature (iteratePrime X k)
+
+end Chromosome
+
+
+section Legacy
+
+local notation "PolarizedList" => List Bool
+
+def List.isAlternating (l : PolarizedList) : Prop :=
+  ∀ i : ℕ, (_ : i < l.length - 1) → l[i] = !l[i + 1]
+
+lemma nil_isAlternating : [].isAlternating := by
+  simp [List.isAlternating]
+
+lemma sing_isAlternating {x : Bool} : [x].isAlternating := by
+  simp [List.isAlternating]
+
+lemma cons_isAlternating {x : Bool} {xs : PolarizedList} (hxs : xs ≠ []) :
+    (x :: xs).isAlternating ↔ x = !xs.head hxs ∧ xs.isAlternating := by
+  constructor
+  · intro h
+    constructor
+    · specialize h 0
+      simp [List.ne_nil_iff_length_pos.1 hxs] at h
+      rw [List.head_eq_getElem, h]
+    intro i hi
+    specialize h (i + 1) ?_
+    · rw [List.length_cons]; omega
+    simpa using h
+  · intro h i hi
+    by_cases hi' : i = 0
+    · simpa [hi', List.head_eq_getElem] using h.1
+    rw [List.length_cons, add_tsub_cancel_right] at hi
+    conv => enter [1, 2]; rw [(Nat.succ_pred_eq_of_ne_zero hi').symm]
+    conv => enter [2, 1, 2]; rw [(Nat.succ_pred_eq_of_ne_zero hi').symm]
+    simp [h.2 (i - 1) (by omega)]
+
+instance {l : PolarizedList} : Decidable l.isAlternating := by
+  induction l with
+  | nil => exact Decidable.isTrue nil_isAlternating
+  | cons x xs h =>
+    match xs with
+    | [] => exact Decidable.isTrue sing_isAlternating
+    | x' :: xs =>
+      exact decidable_of_decidable_of_iff (cons_isAlternating (xs.cons_ne_nil x')).symm
+
+#eval [true].isAlternating
+#eval [true, true].isAlternating
+#eval [true, false].isAlternating
+#eval [true, false, true].isAlternating
+
+def PolarizedList.toGene (l : PolarizedList)
+    (_ : l.isAlternating := by decide) (hlen : 1 ≤ l.length := by decide) : Gene :=
+  ⟨l.length, if l[0] = true then .Positive else .Negative, hlen⟩
+
+#eval PolarizedList.toGene [true, false, true]
+#eval PolarizedList.toGene [true, false]
+#eval PolarizedList.toGene [true]
+
+#eval List.iterate not false 3
+
+def PolarizedGene.toList (g : Gene) (_ : g.type ≠ .NonPolarized := by decide) :
+    PolarizedList :=
+  match g.type with
+  | .Positive => List.iterate not true g.rank
+  | .Negative => List.iterate not false g.rank
+  | .NonPolarized => by tauto
+
+end Legacy
