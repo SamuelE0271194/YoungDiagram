5c729e0aeba36ab43f31575104e8e1954a771970
Yu-Misaka <109365723+Yu-Misaka@users.noreply.github.com>
2026-02-02 16:35:05 +0800

    lemma 9 statement fix

--- YoungDiagram/Lifting.lean
+++ YoungDiagram/Lifting.lean
@@ -3,20 +3,21 @@ import YoungDiagram.Mutations
 
 open Chromosome
 
-variable {idx : Fin 5} {k : ℕ}
+variable (idx : Fin 5) (k : ℕ)
 
-local notation "φ"  => VarietyLabel idx
-local notation "φᵏ" => VarietyLabel <| VarietyLabel.prime^[k] idx
+noncomputable abbrev φ := VarietyLabel idx
+noncomputable abbrev φk := VarietyLabel (VarietyLabel.prime^[k] idx)
 
-section lifting_property
+variable {X U : Chromosome} (hX : X ∈ φ idx) (hU : U ∈ φk idx k)
 
-variable {X U : Chromosome} (hX : X ∈ φ) (hU : U ∈ φᵏ)
-variable (hMu : MutationStep (VarietyLabel.prime^[k] idx)
-    (VarietyLabel.of_mem_prime_iter hX) ⟨U, hU⟩)
+noncomputable abbrev LiftingMutationStep :=
+  MutationStep (VarietyLabel.prime^[k] idx)
+    (VarietyLabel.of_mem_prime_iter hX) ⟨U, hU⟩
 
-lemma lifting_property : ∃ Z : Chromosome, (hZ : Z ∈ φ) →
+variable (hMu : LiftingMutationStep idx k hX hU)
+
+include hU hMu in
+lemma lifting_property : ∃ Z : Chromosome, (hZ : Z ∈ φ idx) →
     MutationStep idx ⟨X, hX⟩ ⟨Z, hZ⟩ ∧
     prime^[k] Z = U ∧
     ∀ i ≤ k, signature (prime^[k] X) = signature (prime^[k] Z) := sorry
-
-end lifting_property
