43c704739972597b449894d4719cd83a6ad1ffaa
Yu-Misaka <109365723+Yu-Misaka@users.noreply.github.com>
2026-02-02 16:38:50 +0800

    streamline

--- YoungDiagram/Lifting.lean
+++ YoungDiagram/Lifting.lean
@@ -7,15 +7,12 @@ noncomputable section
 variable (idx : Fin 5) (k : ℕ)
 
 abbrev φ := VarietyLabel idx
-abbrev φk := VarietyLabel (VarietyLabel.prime^[k] idx)
+abbrev ψ := VarietyLabel (VarietyLabel.prime^[k] idx)
 
-variable {X U : Chromosome} (hX : X ∈ φ idx) (hU : U ∈ φk idx k)
+variable {X U : Chromosome} (hX : X ∈ φ idx) (hU : U ∈ ψ idx k)
 
-abbrev LiftingMutationStep :=
-  MutationStep (VarietyLabel.prime^[k] idx)
-    (VarietyLabel.of_mem_prime_iter hX) ⟨U, hU⟩
-
-variable (hMu : LiftingMutationStep idx k hX hU)
+variable (hMu : MutationStep (VarietyLabel.prime^[k] idx)
+    (VarietyLabel.of_mem_prime_iter hX) ⟨U, hU⟩)
 
 include hU hMu in
 lemma lifting_property : ∃ Z : Chromosome, (hZ : Z ∈ φ idx) →
